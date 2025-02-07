b9623c314e4be0592c9f6686f54e92e7c65dbcfc
L4pki <79217462+L4pki@users.noreply.github.com>
2025-02-07 14:22:41 +0300

    + форма регистрации (внешка)

--- src/components/AuthForm/RegistrationForm/RegistrationForm.tsx
+++ src/components/AuthForm/RegistrationForm/RegistrationForm.tsx
@@ -1,22 +1,42 @@
 import React, { useState } from "react";
-import { UserAuthProps } from "../../../types/User"
-import logo from '../../../assets/images/logo.png'
-import "./RegistrationForm.scss"
+import { UserAuthProps } from "../../../types/User";
+import "./RegistrationForm.scss";
 
 const RegistrationForm: React.FC = () => {
-    const [user, setUser] = useState<UserAuthProps>({
-        email: "",
-        password: ""
-    });
+  const [user, setUser] = useState<UserAuthProps>({
+    email: "",
+    password: "",
+  });
 
-    return (
-      <div className="registration-form-container">
+  return (
+    <div className="registration-form-container">
+      <div className="registration-form-content">
         <div className="registration-form-title-block">
-          <img className="registration-form-title-logo" src={logo} alt="логотип" />
           <p className="registration-form-title-text">DyDaI LoX</p>
+          <p className="registration-form-title-text-description">
+            Created by VT
+          </p>
+        </div>
+        <div className="registration-form-input-block">
+          <input
+            className="registration-form-input-block-element"
+            type="text"
+            placeholder="email"
+          />
+          <input
+            className="registration-form-input-block-element"
+            type="text"
+            placeholder="password"
+          />
+          <input
+            className="registration-form-input-block-element"
+            type="text"
+            placeholder="login"
+          />
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default RegistrationForm;
--- src/pages/AuthorizationPage/AuthPage.tsx
+++ src/pages/AuthorizationPage/AuthPage.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AuthPage.scss";
 import RegistrationForm from "../../components/AuthForm/RegistrationForm/RegistrationForm";
 
 const AuthPage: React.FunctionComponent = () => {
+  const [isLoginMode, setIsLoginMode] = useState<boolean>(false);
+
   return (
     <div className="auth-page">
       <div className="auth-page-container">
-        <RegistrationForm />
+        {isLoginMode === false && (
+          <>
+            <RegistrationForm />
+            <p className="goto-login" onClick={()=> setIsLoginMode(true)}>Уже есть аккаунт?</p>
+          </>
+        )}
       </div>
     </div>
   );
